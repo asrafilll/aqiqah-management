5ecc6335b629a556e21740b0c0fe5713b980e667
Ilhammeru <gumilang.dev@gmail.com>
2022-03-21 19:14:38 +0700

    fix bugs

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,9 +11,9 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    public function storeFile($folder, $file){
+    public function storeFile($folder, $file, $name){
 		$filename = rand(1, 99999999) . '.' . $file->getClientOriginalExtension();
-		$file->move(public_path("uploaded_files/{$folder}"), $filename);
-		return $filename;
+		$file->move(public_path("uploaded_files/{$folder}"), $name);
+		return $name;
 	}
 }
