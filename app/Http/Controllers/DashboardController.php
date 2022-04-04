a5d6190c45f0d9db3c641414553c80e07650bbac
Muhammad Syachrul <syachrulmuhammad@gmail.com>
2022-04-04 21:09:33 +0700

    fix: only show relevant dashboard based on user branch

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order\Order;
+use App\Models\UsersBranch;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
@@ -14,19 +16,36 @@ class DashboardController extends Controller
         // 2: Facebook
         // 3: Google
         // 4: Others
+        /** @var \App\User */
+        $user = Auth::user();
+        $userBranch = UsersBranch::query()
+            ->where('users_id', $user->id)
+            ->first();
         $formatIdrNumber = function ($number) {
             return number_format($number, 0, ',', '.');
         };
-        $countOrdersBySourceOrderId = function ($sourceOrderId) use ($formatIdrNumber) {
+        $countOrders = function () use ($formatIdrNumber, $userBranch) {
             return $formatIdrNumber(
                 Order::query()
+                    ->when(!is_null($userBranch), function ($query) use ($userBranch) {
+                        $query->where('branch_id', $userBranch->branch_id);
+                    })
+                    ->count()
+            );
+        };
+        $countOrdersBySourceOrderId = function ($sourceOrderId) use ($formatIdrNumber, $userBranch) {
+            return $formatIdrNumber(
+                Order::query()
+                    ->when(!is_null($userBranch), function ($query) use ($userBranch) {
+                        $query->where('branch_id', $userBranch->branch_id);
+                    })
                     ->where('source_order_id', $sourceOrderId)
                     ->count()
             );
         };
 
         return view('dashboard', [
-            'totalOrders' => $formatIdrNumber(Order::query()->count()),
+            'totalOrders' => $countOrders(),
             'totalInstagramOrders' => $countOrdersBySourceOrderId(1),
             'totalFacebookOrders' => $countOrdersBySourceOrderId(2),
             'totalGoogleOrders' => $countOrdersBySourceOrderId(3),
