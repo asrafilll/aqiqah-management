814040b3a3140ee479274be11002370851bb0adf
Muhammad Syachrul <syachrulmuhammad@gmail.com>
2022-04-04 22:16:11 +0700

    fix: adjustment brand logo

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -66,7 +66,7 @@
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
-      <a href="{{ url('/') }}" class="brand-link" style="height: 100px;">
+      <a href="{{ url('/') }}" class="brand-link" style="padding: 20px 0">
         <img src="{{asset('img/logo.png')}}" alt="Syamil Aqiqah" class="brand-image img-circle" style="width: 60px;max-height: 60px;">
         <p class="brand-text mt-2" style="font-style: normal;font-size: 19px;letter-spacing: 0.4px;color: #FAFAFA;opacity: 0.7;">Syamil Aqiqah</p>
       </a>
